23064ba21fbf3088962fae5236f2a69cb67f8334
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2024-07-28 09:34:52 -0300

    Curso ASP.NET

---
+++ Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendas.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime UltimaCompra { get; set; }
+        public float Estoque { get; set; }
+    }
+}
